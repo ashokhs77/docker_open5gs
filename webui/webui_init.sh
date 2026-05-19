6d71833fa4a9925518c139d62dd19beabc03025e
ashokhs77 <ashokhs77@gmail.com>
2026-05-19 15:13:14 +0530

    using proper versions of webui docker and init files

--- webui/webui_init.sh
+++ webui/webui_init.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
-set -euo pipefail
 
-if [[ -z "${MONGO_IP:-}" ]]; then
-    echo "Error: MONGO_IP environment variable not set"
-    exit 1
-fi
+# BSD 2-Clause License
 
-export DB_URI="${DB_URI:-mongodb://${MONGO_IP}/open5gs}"
-export HOSTNAME="${HOSTNAME:-0.0.0.0}"
-export PORT="${PORT:-9999}"
+# Copyright (c) 2020-2025, Supreeth Herle
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Open5GS WebUI init script (5G SA deployments only).
+# This is loaded only from sa-vonr-deploy.yaml. It is never referenced by
+# 4g-volte-deploy.yaml and has no effect on 4G deployments.
+
+export DB_URI="mongodb://${MONGO_IP}/open5gs"
+
+# Wait for mongo to accept connections
+for i in $(seq 1 30); do
+    if (echo > /dev/tcp/${MONGO_IP}/27017) >/dev/null 2>&1; then
+        echo "MongoDB at ${MONGO_IP}:27017 is reachable"
+        break
+    fi
+    echo "Waiting for MongoDB at ${MONGO_IP}:27017... (${i}/30)"
+    sleep 2
+done
 
 cd /open5gs/webui
-exec npm run dev
+exec npm run dev -- --host 0.0.0.0 --port 9999 $@
